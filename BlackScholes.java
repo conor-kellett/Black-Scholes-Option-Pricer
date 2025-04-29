1867bc1d8609b5015483695c53706150b21aa2a7
Conor Kellett <conorplayz11@gmail.com>
2025-04-29 16:56:41 +1000

    Created a public class called BlackScholes which implements the PriceType interface and has its own implementation of the getCall and getPut methods.

---
+++ BlackScholes.java
@@ -0,0 +1,16 @@
+public class BlackScholes implements PriceType {
+
+    @Override
+    public double getCall() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCall'");
+    }
+
+    @Override
+    public double getPut() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPut'");
+    }
+
+    
+}
